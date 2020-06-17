ebc41182006c631ce870f2bc1e1fa5f3e5d6b644
SkullFire <gabriel.marques.andrade@gmail.com>
2020-06-17 00:26:07 -0300

    add api interceptor

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,24 @@
 import axios from 'axios';
-
+import {AsyncStorage} from 'react-native'
 const api = axios.create({
-    baseURL: 'http://localhost/api'
+    baseURL: 'https://af2aa2d75cd4.ngrok.io/api'
 });
+
+api.interceptors.request.use(async (config) => {
+    const userToken = AsyncStorage.getItem('access_token');
+    config.headers.Authorization = `Bearer ${userToken}`;
+    return config;
+});
+
+const onResponseSuccess = response => {
+    return response;
+};
+
+const onResponseFail = error => {
+    const status = error.status || error.response.status;
+    return Promise.reject(error);
+};
+
+api.interceptors.response.use(onResponseSuccess, onResponseFail);
 
 export default api;
